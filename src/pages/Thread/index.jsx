739b3a174b6c1428987d97efd2a27d822f5f864b
odendayoko <128268777+odendayoko@users.noreply.github.com>
2024-04-14 22:28:56 +0900

    スレッド内の投稿一覧ページ追加

--- src/pages/Thread/index.jsx
+++ src/pages/Thread/index.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
 import { ThreadPagePresenter } from "./presenter";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 export const ThreadPage = () => {
-  return <ThreadPagePresenter />;
+  const [posts, setPosts] = useState([]);
+
+  const { thread_id } = useParams();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(
+          `https://railway.bulletinboard.techtrain.dev/threads/${thread_id}/posts`,
+          { params: { offset: 1 } }
+        );
+
+        setPosts(response.data.posts);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchPosts();
+  }, [thread_id]);
+
+  return <ThreadPagePresenter posts={posts} />;
 };
--- src/pages/Thread/presenter.jsx
+++ src/pages/Thread/presenter.jsx
@@ -1,3 +1,25 @@
-export const ThreadPagePresenter = () => {
-  return <div>ほげ</div>;
+import { Header } from "../../components/commons/Header";
+import "./Thread.css";
+
+export const ThreadPagePresenter = ({ posts }) => {
+  return (
+    <>
+      <Header />
+      <div className="threadContainer">
+        <div className="threadTitle">タイトルどうする？？</div>
+        <div className="threadBody">
+          <div className="postMain">
+            {posts.map((post) => (
+              <div key={post.id} className="postListItem">
+                {post.post}
+              </div>
+            ))}
+          </div>
+          <div className="newPostFrom">
+            <form></form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
